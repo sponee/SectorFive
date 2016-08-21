7c9981044cd050d33baef324aa216369738be110
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-08-20 20:08:15 -0700

    ship moving on sceen

---
+++ SectorFive/sector_five.rb
@@ -0,0 +1,24 @@
+require 'gosu'
+require_relative 'player'
+
+class SectorFive < Gosu::Window
+  def initialize
+    super(800, 600)
+    self.caption = 'Sector Five'
+    @player = Player.new(self)
+  end
+
+  def draw
+    @player.draw
+  end
+
+  def update
+    @player.turn_left if button_down?(Gosu::KbLeft)
+    @player.turn_right if button_down?(Gosu::KbRight)
+    @player.accelerate if button_down?(Gosu::KbUp)
+    @player.move
+  end
+end
+
+window = SectorFive.new
+window.show
